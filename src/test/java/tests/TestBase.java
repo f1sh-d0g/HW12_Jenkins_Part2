dfd96af9e114bda956e4ee5f1661d326a2dbbeee
DorovkovDA <dmitry.borovkov@gmail.com>
2025-03-10 11:46:34 +0300

    cloned hw11

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,28 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+        @BeforeAll
+        static void setupConfiguration() {
+            Configuration.browserSize = "1920x2160";
+            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.pageLoadStrategy = "eager";
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+            SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+}
