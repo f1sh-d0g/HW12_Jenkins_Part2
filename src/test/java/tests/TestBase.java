a04e474a14f5f5944bb5cf6b4c6861d0b426b904
DorovkovDA <dmitry.borovkov@gmail.com>
2025-03-10 13:22:23 +0300

    cloned hw11

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -11,7 +11,7 @@ public class TestBase {
         @BeforeAll
         static void setupConfiguration() {
             Configuration.browserSize = "1920x2160";
-            Configuration.baseUrl = "https://demoqa.com";
+            Configuration.baseUrl = System.getProperty("baseUrl", "https://demoqa.com");
             Configuration.pageLoadStrategy = "eager";
             Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
             SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
